dc79db62ea40800d79351ec233995ca62075ae7a
Liang-Bo Wang <me@liang2.tw>
2016-06-21 16:19:04 +0800

    Add base report and stage

--- bc_pipelines/base/report.py
+++ bc_pipelines/base/report.py
@@ -1,5 +1,18 @@
-from bc_report.report import Report
+from pathlib import Path
+from bc_report.report import Report, Stage, SummaryStage
+
+here = Path(__file__).parent
+
+
+class BaseSummaryHomeStage(SummaryStage):
+    template_entrances = ['index.html']
+    template_find_paths = [
+        here / 'templates',
+    ]
 
 
 class BaseReport(Report):
-    pass
+    stage_classes = [BaseSummaryHomeStage]
+    static_roots = [
+        here / 'static',
+    ]
