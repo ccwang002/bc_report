14f48c5f214ab3cb04a587035b86565832d7032c
Liang-Bo Wang <me@liang2.tw>
2016-06-23 15:57:35 +0800

    RNASeq add FastQC stage

--- bc_pipelines/rna_seq/report.py
+++ bc_pipelines/rna_seq/report.py
@@ -1,19 +1,28 @@
 from pathlib import Path
+from bc_report.report import Stage
 from ..base.report import BaseSummaryHomeStage, BaseReport
+from ..base.fastqc import FastQCStage
 
 here = Path(__file__).parent
 
 
-class RNASeqSummaryHomeStage(BaseSummaryHomeStage):
+class RNASeqStageMixin(Stage):
     template_find_paths = [
         here / 'templates',
         *BaseSummaryHomeStage.template_find_paths,
     ]
+
+
+class RNASeqFastQCStage(RNASeqStageMixin,FastQCStage):
+    template_entrances = ['rna_seq/fastqc.html']
+
+
+class RNASeqSummaryHomeStage(RNASeqStageMixin, BaseSummaryHomeStage):
     template_entrances = ['rna_seq/index.html']
 
 
 class RNASeqReport(BaseReport):
-    stage_classes = [RNASeqSummaryHomeStage]
+    stage_classes = [RNASeqSummaryHomeStage, RNASeqFastQCStage]
     static_roots = [
         here / 'static',
         *BaseReport.static_roots,
